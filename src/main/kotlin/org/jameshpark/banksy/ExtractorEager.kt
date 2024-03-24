e87676787979fdf1ecc8966648762c6fae6fa5d7
James Park <parkjames91@gmail.com>
2024-03-24 15:55:43 -0400

    Adds ExtractorEager for testing

---
+++ src/main/kotlin/org/jameshpark/banksy/ExtractorEager.kt
@@ -0,0 +1,37 @@
+package org.jameshpark.banksy
+
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import org.jameshpark.org.jameshpark.banksy.models.Transaction
+import org.jameshpark.org.jameshpark.banksy.models.toTransaction
+import java.io.File
+
+object ExtractorEager {
+
+    fun extractFromCsv(directoryName: String): List<Transaction> {
+        val rows = extractRows(directoryName)
+        return parseTransactions(rows)
+    }
+
+    private fun extractRows(directoryName: String): List<Map<String, String>> {
+        val reader = csvReader()
+        val directory = File(directoryName)
+
+        return if (directory.exists() && directory.isDirectory) {
+            directory.walk().toList().filter { it.isFile && it.extension.lowercase() == "csv" }.flatMap { file ->
+                reader.readAllWithHeader(file)
+            }
+        } else {
+            throw IllegalArgumentException("Directory '$directoryName' does not exist or is not a directory")
+        }
+    }
+
+    private fun parseTransactions(rows: List<Map<String, String>>): List<Transaction> = rows.map { row ->
+        val mapper = headersToMapper[row.keys]
+        if (mapper != null) {
+            row.toTransaction(mapper)
+        } else {
+            println("No mapper for headers '${row.keys}', skipping row '${row.values}'")
+            null
+        }
+    }.filterNotNull().sortedBy { it.description }
+}
--- src/main/kotlin/org/jameshpark/banksy/Main.kt
+++ src/main/kotlin/org/jameshpark/banksy/Main.kt
@@ -2,8 +2,11 @@ package org.jameshpark.org.jameshpark.banksy
 
 import kotlinx.coroutines.runBlocking
 import org.jameshpark.banksy.Extractor
+import org.jameshpark.banksy.ExtractorEager
 
 fun main() = runBlocking {
-    val transactions = Extractor.extractFromCsv("transactions")
-    transactions.collect { println(it) }
+//    val transactions = Extractor.extractFromCsv("transactions")
+//    transactions.collect { println(it) }
+    val transactions = ExtractorEager.extractFromCsv("transactions")
+    transactions.forEach { println(it) }
 }
