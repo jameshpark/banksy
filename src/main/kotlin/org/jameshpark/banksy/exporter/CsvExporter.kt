4d821ae896ff23ac65a0fc54bb7f1e22d200add1
James Park <parkjames91@gmail.com>
2024-06-01 18:38:57 -0400

    Makes CsvExporter smarter:
    CsvExporter now won't create a file unless there's something to export.

--- src/main/kotlin/org/jameshpark/banksy/exporter/CsvExporter.kt
+++ src/main/kotlin/org/jameshpark/banksy/exporter/CsvExporter.kt
@@ -1,6 +1,8 @@
 package org.jameshpark.banksy.exporter
 
+import com.github.doyaaaaaken.kotlincsv.client.CsvFileWriter
 import com.github.doyaaaaaken.kotlincsv.client.CsvWriter
+import com.github.doyaaaaaken.kotlincsv.client.KotlinCsvExperimental
 import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.Dispatchers
@@ -17,38 +19,57 @@ class CsvExporter(
 
     override suspend fun export(sink: CsvSink, sinceId: Int) {
         val filePath = sink.filePath
-        val includeHeader = sink.includeHeader
-
-        logger.info { "Exporting to $filePath" }
-        val output = File(filePath)
-
-        // create new file
-        withContext(Dispatchers.IO) {
-            output.parentFile.mkdirs()
-            output.createNewFile()
-            if (includeHeader) {
-                output.writeText("date,description,amount,category,critical,type,originHash\n")
-            }
-        }
-
         val transactions = dao.getTransactionsNewerThanId(sinceId)
 
-        val counter = AtomicInteger(0)
-        writer.openAsync(output, append = true) {
-            transactions.collect {
+        var output: File? = null
+        var csvFileWriter: CsvFileWriter? = null
+        var counter: AtomicInteger? = null
+
+        try {
+            transactions.collect { transaction ->
+                output = setOrGetFile(output, filePath, sink.includeHeader)
+                csvFileWriter = setOrGetWriter(csvFileWriter, output!!)
+                counter = setOrGetCounter(counter)
+
                 withContext(Dispatchers.IO) {
-                    writeRow(it.toCsvRow())
+                    csvFileWriter!!.writeRow(transaction.toCsvRow())
                 }
-                if (counter.incrementAndGet() % 10 == 0) {
-                    logger.info { "Exported ${counter.get()} transactions to $filePath" }
+
+                if (counter!!.incrementAndGet() % 10 == 0) {
+                    logger.info { "Exported ${counter!!.get()} transactions to $filePath" }
+                }
+            }
+
+            logger.info { "Exported ${counter!!.get()} transactions to $filePath" }
+        } finally {
+            csvFileWriter?.close()
+        }
+
+        if (output == null) {
+            logger.info { "No new transactions to export to csv." }
+        }
+    }
+
+    private suspend fun setOrGetFile(output: File?, filePath: String, includeHeader: Boolean) =
+        output ?: withContext(Dispatchers.IO) {
+            File(filePath).apply {
+                parentFile.mkdirs()
+                createNewFile()
+                if (includeHeader) {
+                    writeText(CSV_HEADER)
                 }
             }
         }
-        logger.info { "Exported ${counter.get()} transactions to $filePath" }
-    }
+
+    @OptIn(KotlinCsvExperimental::class)
+    private fun setOrGetWriter(csvFileWriter: CsvFileWriter?, output: File) =
+        csvFileWriter ?: writer.openAndGetRawWriter(output, append = true)
+
+    private fun setOrGetCounter(counter: AtomicInteger?) = counter ?: AtomicInteger(0)
 
     companion object {
         private val logger = KotlinLogging.logger { }
+        private const val CSV_HEADER = "date,description,amount,category,critical,type,originHash\n"
     }
 
 }
