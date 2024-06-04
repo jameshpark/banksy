c2794bdc6906a26d125292f642ab7a9a42bb6f61
James Park <parkjames91@gmail.com>
2024-06-03 23:44:22 -0400

    Adds handling of access + refresh tokens expiring

--- src/main/kotlin/org/jameshpark/banksy/utils/GoogleSheetsExt.kt
+++ src/main/kotlin/org/jameshpark/banksy/utils/GoogleSheetsExt.kt
@@ -1,6 +1,7 @@
 package org.jameshpark.banksy.utils
 
 import com.google.api.client.auth.oauth2.Credential
+import com.google.api.client.auth.oauth2.TokenResponseException
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow
@@ -11,11 +12,15 @@ import com.google.api.client.util.store.FileDataStoreFactory
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.SheetsScopes
 import com.google.api.services.sheets.v4.model.ValueRange
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.StringReader
 import java.util.*
+
+private val logger = KotlinLogging.logger { }
+private val LOCAL_TOKENS_DIR = File("tokens")
 
 suspend fun Sheets.appendRows(spreadsheetId: String, sheetName: String, rows: List<List<Any>>) {
     val firstEmptyRow = getFirstEmptyRow(spreadsheetId, sheetName)
@@ -47,16 +52,32 @@ fun sheetsServiceFromProperties(properties: Properties): Sheets {
 
 fun sheetsServiceFromSecretJson(secretJson: String): Sheets {
     val clientSecrets = loadGoogleClientSecrets(secretJson)
-    val credential = getUserAuthorization(clientSecrets)
 
-    return Sheets.Builder(
+    return buildSheetsService(clientSecrets).let {
+        try {
+            it.spreadsheets().get("FAKE_SPREADSHEET_ID").execute()
+            it
+        } catch (e: TokenResponseException) {
+            logger.warn { "Both access and refresh tokens have expired. Purging and reauthenticating.." }
+            if (LOCAL_TOKENS_DIR.exists()) LOCAL_TOKENS_DIR.deleteRecursively()
+            buildSheetsService(clientSecrets)
+
+        } catch (t: Throwable) {
+            // This means our request to Google was authorized and
+            // couldn't find the fake spreadsheet. Yay!
+            it
+        }
+    }
+}
+
+private fun buildSheetsService(clientSecrets: GoogleClientSecrets): Sheets =
+    Sheets.Builder(
         GoogleNetHttpTransport.newTrustedTransport(),
         GsonFactory.getDefaultInstance(),
-        credential
+        getUserAuthorization(clientSecrets)
     )
         .setApplicationName("Banksy Kotlin App")
         .build()
-}
 
 private fun loadGoogleClientSecrets(credentialsJson: String): GoogleClientSecrets {
     val jsonFactory = GsonFactory.getDefaultInstance()
@@ -72,7 +93,7 @@ private fun getUserAuthorization(clientSecrets: GoogleClientSecrets): Credential
         clientSecrets,
         listOf(SheetsScopes.SPREADSHEETS)
     )
-        .setDataStoreFactory(FileDataStoreFactory(File("tokens")))
+        .setDataStoreFactory(FileDataStoreFactory(LOCAL_TOKENS_DIR))
         .setAccessType("offline")
         .build()
     val receiver = LocalServerReceiver.Builder().setPort(8888).build()
